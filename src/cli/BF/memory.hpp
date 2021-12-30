00504c4ab8b88f9707ec5e9219cf9473a4d8d97e
pum1k <55055380+pum1k@users.noreply.github.com>
2021-12-30 18:43:25 +0100

    Create BF memory header
    - declarations only
    - classes to handle brainfuck memory

---
+++ src/cli/BF/memory.hpp
@@ -0,0 +1,78 @@
+#ifndef CLI_BF_MEMORY_HPP_
+#define CLI_BF_MEMORY_HPP_
+
+#include <algorithm>
+#include <stdexcept>
+
+template <class T>
+class BFMemoryBase
+{
+ protected:
+    BFMemoryBase(){};
+
+ public:
+    virtual ~BFMemoryBase(){};
+    BFMemoryBase(const BFMemoryBase<T> &) = delete;
+    BFMemoryBase(BFMemoryBase<T> &&)      = delete;
+    BFMemoryBase &operator=(const BFMemoryBase<T> &) = delete;
+    BFMemoryBase &operator=(BFMemoryBase<T> &&) = delete;
+
+    virtual void inc_val() = 0;
+    virtual void dec_val() = 0;
+    virtual void inc_ptr() = 0;
+    virtual void dec_ptr() = 0;
+    virtual T &ref()       = 0;
+};
+
+template <class T>
+class BFMemoryStaticBase : public BFMemoryBase<T>
+{
+ protected:
+    T *arr;
+    T *ptr;
+    std::size_t size_;
+
+    BFMemoryStaticBase(std::size_t size);
+
+ public:
+    virtual ~BFMemoryStaticBase() override;
+
+    virtual void inc_val() override;
+    virtual void dec_val() override;
+    virtual T &ref() override;
+};
+
+template <class T>
+class BFMemoryStaticUnsafe : public BFMemoryStaticBase<T>
+{
+ protected:
+ public:
+    BFMemoryStaticUnsafe(std::size_t size) : BFMemoryStaticBase<T>(size) {}
+
+    virtual void inc_ptr() override;
+    virtual void dec_ptr() override;
+};
+
+template <class T>
+class BFMemoryStaticSafe : public BFMemoryStaticBase<T>
+{
+ protected:
+ public:
+    BFMemoryStaticSafe(std::size_t size) : BFMemoryStaticBase<T>(size) {}
+
+    virtual void inc_ptr() override;
+    virtual void dec_ptr() override;
+};
+
+template <class T>
+class BFMemoryStaticLoop : public BFMemoryStaticBase<T>
+{
+ protected:
+ public:
+    BFMemoryStaticLoop(std::size_t size) : BFMemoryStaticBase<T>(size) {}
+
+    virtual void inc_ptr() override;
+    virtual void dec_ptr() override;
+};
+
+#endif // CLI_BF_MEMORY_HPP_
