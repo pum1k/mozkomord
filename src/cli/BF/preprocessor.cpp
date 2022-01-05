894f2b60c3c8b6142878601bb2702a2701d192c6
pum1k <55055380+pum1k@users.noreply.github.com>
2022-01-05 22:01:20 +0100

    Change BF preprocessor
    - add private function pointers to functions checking and processing
      the program
    - add public methods for setting these functions

--- src/cli/BF/preprocessor.cpp
+++ src/cli/BF/preprocessor.cpp
@@ -6,3 +6,22 @@ void BFPreprocessor::set_program(const std::vector<char> &prog)
 }
 
 const std::vector<char> &BFPreprocessor::get_program() { return this->program; }
+
+void BFPreprocessor::set_check(
+    const std::function<void(std::vector<char> &)> &func)
+{
+    this->check_ = func;
+}
+
+void BFPreprocessor::set_process(
+    const std::function<void(std::vector<char> &, bool)> &func)
+{
+    this->process_ = func;
+}
+
+void BFPreprocessor::check() { return this->check_(this->program); }
+
+void BFPreprocessor::process(bool keep_debug /* = true */)
+{
+    return this->process_(this->program, keep_debug);
+}
--- src/cli/BF/preprocessor.hpp
+++ src/cli/BF/preprocessor.hpp
@@ -1,6 +1,7 @@
 #ifndef CLI_BF_PREPROCESSOR_HPP_
 #define CLI_BF_PREPROCESSOR_HPP_
 
+#include <functional>
 #include <vector>
 
 class BFPreprocessor
@@ -8,14 +9,19 @@ class BFPreprocessor
  protected:
     std::vector<char> program;
 
+    std::function<void(std::vector<char> &)> check_;
+    std::function<void(std::vector<char> &, bool)> process_;
+
  public:
-    virtual ~BFPreprocessor() {}
-
     void set_program(const std::vector<char> &prog);
-    virtual const std::vector<char> &get_program();
+    const std::vector<char> &get_program();
 
-    virtual void check()                         = 0;
-    virtual void process(bool keep_debug = true) = 0;
+    void set_check(const std::function<void(std::vector<char> &)> &func);
+    void set_process(
+        const std::function<void(std::vector<char> &, bool)> &func);
+
+    void check();
+    void process(bool keep_debug = true);
 };
 
 #endif // CLI_BF_PREPROCESSOR_HPP_
