50d020e7157b3fa1741c73e891cdb32ee548db1f
pum1k <55055380+pum1k@users.noreply.github.com>
2022-01-09 21:41:23 +0100

    Create BF exceptions
    - BF::syntax_error - generic syntax error
    - BF::unexpected_bracket_error - unexpected closing bracket

---
+++ src/cli/BF/except.cpp
@@ -0,0 +1,29 @@
+#include "except.hpp"
+
+namespace BF
+{
+void unexpected_bracket_error::init_msg()
+{
+    if (!this->has_pos())
+    {
+        this->msg = "Unexpected closing bracket.";
+        return;
+    }
+    this->msg = "Unexpected closing bracket at line: ";
+    this->msg += std::to_string(this->pos.first);
+    this->msg += ", char: ";
+    this->msg += std::to_string(this->pos.second);
+    this->msg += '.';
+}
+
+const char *unexpected_bracket_error::what() const noexcept
+{
+    return this->msg.c_str();
+}
+
+bool unexpected_bracket_error::has_pos() const noexcept
+{
+    return !(this->pos.first == 0 || this->pos.second == 0);
+}
+
+} // namespace BF
---
+++ src/cli/BF/except.hpp
@@ -0,0 +1,42 @@
+#ifndef CLI_BF_EXCEPT_HPP_
+#define CLI_BF_EXCEPT_HPP_
+
+#include <exception>
+#include <string>
+#include <utility>
+
+namespace BF
+{
+class syntax_error : public std::exception
+{
+ public:
+    virtual const char *what() const noexcept override
+    {
+        return "BF::syntax_error";
+    };
+};
+
+class unexpected_bracket_error : public syntax_error
+{
+ private:
+    std::pair<int, int> pos;
+    std::string msg;
+    void init_msg();
+
+ public:
+    unexpected_bracket_error() : pos({0, 0}) { this->init_msg(); };
+    unexpected_bracket_error(int line, int col) : pos({line, col})
+    {
+        this->init_msg();
+    };
+    unexpected_bracket_error(const std::pair<int, int> &pos) : pos(pos){};
+
+    virtual const char *what() const noexcept override;
+
+    bool has_pos() const noexcept;
+    const std::pair<int, int> &get_pos() const noexcept { return pos; };
+};
+
+} // namespace BF
+
+#endif // CLI_BF_EXCEPT_HPP_
